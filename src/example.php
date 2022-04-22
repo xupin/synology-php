e0126d4f9c24843d9201277e60aac8baa13f1fe6
mikespub <github@mikespub.net>
2022-04-22 15:47:03 +0200

    switch to psr-4

--- src/example.php
+++ src/example.php
@@ -2,17 +2,7 @@
 /**
  * This file is an example on how to use Synology\Api
  */
-/*
-set_include_path(dirname(__FILE__) . '/src' . PATH_SEPARATOR . get_include_path());
-*/
-function __autoload($class_name)
-{
-    $path = str_replace('\\', DIRECTORY_SEPARATOR, $class_name);
-    include $path . '.php';
-}
-/*
-require __DIR__ . '/vendor/autoload.php';
-*/
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 $synology = new Synology\Api('192.168.10.5', 5001, 'https', 1);
 $synology->activateDebug();
